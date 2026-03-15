67fb2c276362dd8ef3f4295938d37aebca5c8814
MalakMohamedAhmed <168978331+MalakMohamedAhmed@users.noreply.github.com>
2026-03-16 01:41:53 +0200

    Change data source and add country filter
    Updated data loading to use the preprocessed Zara sales dataset and added a country filter to the sidebar.

--- Zara_Dashboard/src/Dashboard.py
+++ Zara_Dashboard/src/Dashboard.py
@@ -8,12 +8,25 @@ st.set_page_config(page_title="Zara Sales Dashboard", layout="wide")
 # Some helper funstions
 @st.cache_data
 def load_data():
-    data = pd.read_csv("youtube_channel_data.csv")
+    data = pd.read_csv("Zara_sales_dataset_preprocessed.csv")
     data['DATE'] = pd.to_datetime(data['DATE'])
     data['NET_SUBSCRIBERS'] = data['SUBSCRIBERS_GAINED'] - data['SUBSCRIBERS_LOST']
     return data
 
+df = pd.read_csv("Zara_sales_dataset_preprocessed.csv")
+
+# sidebar
 st.sidebar.header('Zara Dashboard')
+
+# countries filter
+countries = ['All'] + sorted(df['Origin'].unique().tolist())
+
+selected_country = st.sidebar.selectbox("Select Country of Origin", countries)
+if selected_country != 'All':
+    filtered_df = df[df['Origin'] == selected_country]
+else:
+    filtered_df = df
+
 
 st.sidebar.markdown('''
 ---
