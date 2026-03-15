8b3e93ac2858fda82da9c446cc9d1b926a31fdfe
MalakMohamedAhmed <168978331+MalakMohamedAhmed@users.noreply.github.com>
2026-03-16 01:35:40 +0200

    Refactor sidebar headers and clean up parameters
    Updated sidebar headers and removed unused parameters for charts.

--- Zara_Dashboard/src/Dashboard.py
+++ Zara_Dashboard/src/Dashboard.py
@@ -13,17 +13,7 @@ def load_data():
     data['NET_SUBSCRIBERS'] = data['SUBSCRIBERS_GAINED'] - data['SUBSCRIBERS_LOST']
     return data
 
-st.sidebar.header('Dashboard `version 2`')
-
-st.sidebar.subheader('Heat map parameter')
-time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
-
-st.sidebar.subheader('Donut chart parameter')
-donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
-
-st.sidebar.subheader('Line chart parameters')
-plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
-plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+st.sidebar.header('Zara Dashboard')
 
 st.sidebar.markdown('''
 ---
