8cc2c6801692fce9236a3c4ad35570ae9be17a20
MalakMohamedAhmed <168978331+MalakMohamedAhmed@users.noreply.github.com>
2026-03-16 01:55:29 +0200

    Add metrics to Dashboard for sales analysis
    Added metrics for average revenue, total units sold, top term, and best season to the dashboard.

--- Zara_Dashboard/src/Dashboard.py
+++ Zara_Dashboard/src/Dashboard.py
@@ -31,3 +31,22 @@ else:
 st.sidebar.markdown('''
 ---
 ''')
+
+# Matrics
+# average revenue
+avg_revenue = filtered_df['revenue'].mean()
+total_units = filtered_df['Sales Volume'].sum()
+
+# season with highest activity
+best_season_vol = filtered_df.groupby('Season')['Sales Volume'].sum().idxmax()
+
+# most sold term
+top_term = filtered_df.groupby('Terms')['Sales Volume'].sum().idxmax()
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Avg Revenue", f"${avg_revenue/1e3:.1f}K")
+col2.metric("Units Sold", f"{total_units:,}")
+col3.metric("Top Term", top_term)
+col4.metric("Best Season", best_season_vol)
+
+st.divider() # Adds a clean line before your charts
