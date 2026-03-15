cd152e5d0ab129ff07299e50983a96a085303975
MalakMohamedAhmed <168978331+MalakMohamedAhmed@users.noreply.github.com>
2026-03-16 01:06:23 +0200

    Update Dashboard.py

--- Zara Dashboard/Dashboard.py
+++ Zara Dashboard/Dashboard.py
@@ -4,3 +4,27 @@ from datetime import timedelta, datetime
 
 # Set page config
 st.set_page_config(page_title="Zara Sales Dashboard", layout="wide")
+
+# Some helper funstions
+@st.cache_data
+def load_data():
+    data = pd.read_csv("youtube_channel_data.csv")
+    data['DATE'] = pd.to_datetime(data['DATE'])
+    data['NET_SUBSCRIBERS'] = data['SUBSCRIBERS_GAINED'] - data['SUBSCRIBERS_LOST']
+    return data
+
+st.sidebar.header('Dashboard `version 2`')
+
+st.sidebar.subheader('Heat map parameter')
+time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
+
+st.sidebar.subheader('Donut chart parameter')
+donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+
+st.sidebar.subheader('Line chart parameters')
+plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+
+st.sidebar.markdown('''
+---
+''')
